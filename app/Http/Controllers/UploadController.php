ffac89fd58a52ac85c3249046dac88f5c2703001
uceuce6 <uswatunchoriyati6@gmail.com>
2021-06-22 20:21:59 +0700

    Beautify and remove random string

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -12,32 +12,34 @@ use RealRashid\SweetAlert\Facades\Alert;
 class UploadController extends Controller
 {
     // public function upload(){
-	// 	return view('upload');
-	// }
+    // 	return view('upload');
+    // }
 
-    public function upload($id){
+    public function upload($id)
+    {
         //$rlp_uploads = rlp_uploads::get();
         $title = "FORM UPLOAD";
         $sub_title = "upload Laporan";
-        $judul='';
+        $judul = '';
         // $daftar=DB::table("rlp_surat_tugas")
         // ->selectRaw("rlp_surat_tugas.no_surat_tugas")
         // ->where("rlp_surat_tugas.id","=",$id)
         // ->first();
 
-        $daftar=DB::table("rlp_surat_tugas")
-        ->selectRaw("rlp_surat_tugas.no_surat_tugas,rlp_laporan_data_umum.nomor_lhp")
-        ->leftJoin("rlp_laporan_data_umum","rlp_surat_tugas.id","=","rlp_laporan_data_umum.rlp_surat_tugas_id")
-        ->where("rlp_surat_tugas.id","=",$id)
-        ->first();
+        $daftar = DB::table("rlp_surat_tugas")
+            ->selectRaw("rlp_surat_tugas.no_surat_tugas,rlp_laporan_data_umum.nomor_lhp")
+            ->leftJoin("rlp_laporan_data_umum", "rlp_surat_tugas.id", "=", "rlp_laporan_data_umum.rlp_surat_tugas_id")
+            ->where("rlp_surat_tugas.id", "=", $id)
+            ->first();
 
         $kode = $id;
-        return view('upload',compact('daftar', 'kode', 'title', 'sub_title', 'judul'));
+        return view('upload', compact('daftar', 'kode', 'title', 'sub_title', 'judul'));
 
 
     }
 
-    public function proses_upload(Request $request){
+    public function proses_upload(Request $request)
+    {
 
         $data = new rlp_uploads();
 
@@ -47,10 +49,9 @@ class UploadController extends Controller
             'file' => 'required',
         ]);
 
-        if($request!=null){
+        if ($request != null) {
             Alert::success('Sukses !', 'Data Berhasil di Upload');
         }
-
 
 
         $rlpSuratTugasId = $request->kode;
@@ -59,32 +60,32 @@ class UploadController extends Controller
         $filePath = $file->getRealPath();
 
         //AssignDataIntoModel
-        $filename = time().'.'.$file->getClientOriginalExtension();
-        $data -> file =$filename;
-        $data -> name = $request->name;
-        $data -> description = $request -> description;
+        $filename = time() . '.' . $file->getClientOriginalExtension();
+        $data->file = $filename;
+        $data->name = $request->name;
+        $data->description = $request->description;
         $data->save;
         //GetFileDetailInfo
         //dd($file);
 
         //MoveFolderOnLocalStorage
         //  $file->move('data_file',$file->getClientOriginalExtension());
-         $file->move('data_file',$file->getClientOriginalName());
+        $file->move('data_file', $file->getClientOriginalName());
 
 
         // $request->user()->proses_upload([
         //     'rlp_uploads'=>$file]);
 
         //Upload to Database
-rlp_uploads::create([
-    'rlp_surat_tugas_id'=>$rlpSuratTugasId,
-    'file_name'=>$fileName,
-    'file_path'=>$filePath,
-    'file_data'=>$file,
-]);
+        rlp_uploads::create([
+            'rlp_surat_tugas_id' => $rlpSuratTugasId,
+            'file_name' => $fileName,
+            'file_path' => $filePath,
+            'file_data' => $file,
+        ]);
 
 // Redirect After Upload
-return Redirect()->back();
+        return Redirect()->back();
 //return redirect('/');
     }
 
@@ -103,39 +104,40 @@ return Redirect()->back();
 
     // }
 
-public function show_data($id){
-    //GetAllData
-    //$data=rlp_uploads::all();
-    //GetSpecificData
-    //$dataUpload=rlp_uploads::find($id);
-    $title = "PREVIEW";
-    $sub_title = "preview Laporan";
-    $judul='';
-
-    $dataUpload=DB::table("rlp_uploads")
-    ->selectRaw("rlp_uploads.id,rlp_uploads.rlp_surat_tugas_id,rlp_uploads.file_name")
-    ->where("rlp_uploads.rlp_surat_tugas_id","=",$id)
-    ->first();
-    // dd($dataUpload);
-
-
-    return view('show_upload', compact('dataUpload', 'title', 'sub_title', 'judul'));
-
-    // $filename = 'test.pdf';
-    // $path = storage_path($filename);
-
-    // return Response::make(file_get_contents($path), 200, [
-    //     'Content-Type' => 'application/pdf',
-    //     'Content-Disposition' => 'inline; filename="'.$filename.'"'
-    // ]);
-//return view('show_file_upload',['data'=>$dataUpload]);
-}
-
-    public function download ($file)
+    public function show_data($id)
     {
-        
+        //GetAllData
+        //$data=rlp_uploads::all();
+        //GetSpecificData
+        //$dataUpload=rlp_uploads::find($id);
+        $title = "PREVIEW";
+        $sub_title = "preview Laporan";
+        $judul = '';
+
+        $dataUpload = DB::table("rlp_uploads")
+            ->selectRaw("rlp_uploads.id,rlp_uploads.rlp_surat_tugas_id,rlp_uploads.file_name")
+            ->where("rlp_uploads.rlp_surat_tugas_id", "=", $id)
+            ->first();
+        // dd($dataUpload);
+
+
+        return view('show_upload', compact('dataUpload', 'title', 'sub_title', 'judul'));
+
+        // $filename = 'test.pdf';
+        // $path = storage_path($filename);
+
+        // return Response::make(file_get_contents($path), 200, [
+        //     'Content-Type' => 'application/pdf',
+        //     'Content-Disposition' => 'inline; filename="'.$filename.'"'
+        // ]);
+//return view('show_file_upload',['data'=>$dataUpload]);
+    }
+
+    public function download($file)
+    {
+
         // return  response()->download(public_path('data_file/'.$file));
-        return response()->file(public_path('data_file/'.$file));
+        return response()->file(public_path('data_file/' . $file));
     }
 
 }
--- resources/views/tables/kap_content.blade.php
+++ resources/views/tables/kap_content.blade.php
@@ -4,10 +4,13 @@
         <div class="col-12">
             <div class="card card-success">
                 <div class="card-header">
-                    <h3 class="card-title"><a class="btn btn-warning" href="{{ route('kapexcel',$pengenal) }}" _blank><i class="fa fa-plus-circle"></i> Cetak EXCEL</a></h3>
+                    <h3 class="card-title"><a class="btn btn-warning" href="{{ route('kapexcel',$pengenal) }}" _blank><i
+                                class="fa fa-plus-circle"></i> Cetak EXCEL</a></h3>
                     <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
-                        <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i
+                                class="fas fa-minus"></i></button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove"><i
+                                class="fas fa-times"></i></button>
 
 
                     </div>
@@ -16,24 +19,23 @@
                 <div class="card-body">
                     <table id="example2" class="table table-bordered table-hover">
                         <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Tema</th>
-                                <th>PKPT</th>
-                                <th>Pelaksana</th>
-                                <th>Surat Tugas</th>
-                                <!-- <th>Laporan</th> -->
-                                <th>Preview</th>
-                                <th></th>
-                            </tr>
+                        <tr>
+                            <th>No</th>
+                            <th>Tema</th>
+                            <th>PKPT</th>
+                            <th>Pelaksana</th>
+                            <th>Surat Tugas</th>
+                            <!-- <th>Laporan</th> -->
+                            <th>Preview</th>
+                            <th></th>
+                        </tr>
                         </thead>
                         <tbody>
-                            @foreach ($daftar as $number => $b)
+                        @foreach ($daftar as $number => $b)
 
                             @php
-n
 
-                            $src = data_table('rlp_uploads','file_name','rlp_surat_tugas_id', '1');
+                                $src = data_table('rlp_uploads','file_name','rlp_surat_tugas_id', '1');
 
                             @endphp
 
@@ -47,26 +49,26 @@ n
                                 <td>{{ $b->nomor_lhp }} <br>{{ $b->judul_lhp }}</td>
                                 <td><a href='{{ url("lihat/{$b->pkpt_id}") }}'>View</a></td>
                                 <td><a href='{{ url("upload/{$b->id_surat_tugas}") }}'>Upload</a></td>
-                                <!-- <td><a href='{{ url("/file/download/{$b->id_surat_tugas}") }}'>Download</a></td> -->
-                                <!-- <td><a href='{{ url("/file/show_data/{$b->id_surat_tugas}") }}' target="_blank">Preview langung</a></td> -->
-                                <!-- <td><a href='{{ url("show_file_upload/{$b->id_surat_tugas}") }}'>Preview    </a></td> -->
-                                <!-- <td><a href='{{ url("show_upload/{$b->id_surat_tugas}") }}'>Preview</a></td> -->
+                            <!-- <td><a href='{{ url("/file/download/{$b->id_surat_tugas}") }}'>Download</a></td> -->
+                            <!-- <td><a href='{{ url("/file/show_data/{$b->id_surat_tugas}") }}' target="_blank">Preview langung</a></td> -->
+                            <!-- <td><a href='{{ url("show_file_upload/{$b->id_surat_tugas}") }}'>Preview    </a></td> -->
+                            <!-- <td><a href='{{ url("show_upload/{$b->id_surat_tugas}") }}'>Preview</a></td> -->
 
                                 <td><a href='{{ url("/file/download/{$src}") }}'>Preview</a></td>
                             </tr>
-                            @endforeach
+                        @endforeach
                         </tbody>
                         <tfoot>
-                            <tr>
-                                <th>No</th>
-                                <th>Tema</th>
-                                <th>PKPT</th>
-                                <th>Pelaksana</th>
-                                <th>Surat Tugas</th>
-                                <!-- <th>Laporan</th> -->
-                                <th>Preview</th>
-                                <th></th>
-                            </tr>
+                        <tr>
+                            <th>No</th>
+                            <th>Tema</th>
+                            <th>PKPT</th>
+                            <th>Pelaksana</th>
+                            <th>Surat Tugas</th>
+                            <!-- <th>Laporan</th> -->
+                            <th>Preview</th>
+                            <th></th>
+                        </tr>
                         </tfoot>
                     </table>
                 </div>
